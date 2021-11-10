68ecf8aca334fe62a0bccc7dd6c75b532aaf8cf5
JinraeKim <kjl950403@gmail.com>
2021-11-10 17:06:40 +0900

    wip

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FSimROS
+
+## Notes
+- ROS2 should be installed.
+- Tested with only few test environments, e.g., Ubuntu 20.04, ROS2 foxy, docker.
+
--- src/convert.jl
+++ src/convert.jl
@@ -1,24 +1,24 @@
 function Base.convert(multicopter::Multicopter, x)
-    geoMsg = pyimport("geometry_msgs.msg")
+    # Msg = pyimport("geometry_msgs.msg")
+    Msg = pyimport("fsim_interfaces.msg")  # TODO
     @unpack p, v, R, ω = x
     quat = dcm_to_quat(SMatrix{3, 3}(R))
     # pose
-    msg_pose = geoMsg.Pose()
-    msg_pose.position.x, msg_pose.position.y, msg_pose.position.z = p
-    msg_pose.orientation.x, msg_pose.orientation.y, msg_pose.orientation.z, msg_pose.orientation.w = quat.q1, quat.q2, quat.q3, quat.q0  # be careful for the order
+    msg = Msg.Pose_and_Twist()
+    msg.pose.position.x, msg.pose.position.y, msg.pose.position.z = p
+    msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w = quat.q1, quat.q2, quat.q3, quat.q0  # be careful for the order
     # twist
-    msg_twist = geoMsg.Twist()
-    msg_twist.linear.x, msg_twist.linear.y, msg_twist.linear.z = v
-    msg_twist.angular.x, msg_twist.angular.y, msg_twist.angular.z = ω
-    msg_pose, msg_twist
+    msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z = v
+    msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z = ω
+    msg
 end
 
-function Base.convert(multicopter::Multicopter, msg_pose, msg_twist)
-    geoMsg = pyimport("geometry_msgs.msg")
-    p = [msg_pose.position.x, msg_pose.position.y, msg_pose.position.z]
-    v = [msg_twist.linear.x, msg_twist.linear.y, msg_twist.linear.z]
-    quat = Quaternion(msg_pose.orientation.w, msg_pose.orientation.x, msg_pose.orientation.y, msg_pose.orientation.z)  # be careful for the order
+function Base.convert(multicopter::Multicopter, msg)
+    Msg = pyimport("fsim_interfaces.msg")
+    p = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
+    v = [msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z]
+    quat = Quaternion(msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z)  # be careful for the order
     R = quat_to_dcm(quat)
-    ω = [msg_twist.angular.x, msg_twist.angular.y, msg_twist.angular.z]
+    ω = [msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z]
     state = State(multicopter)(p, v, R, ω)
 end
